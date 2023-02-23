2d16683d551ea5220986524d541abffeef0d594d
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-23 12:33:23 +0700

    BUILT IN TYPES

---
+++ index.ts
@@ -0,0 +1,25 @@
+// NUMBER TYPE
+let age = 24
+let weight :number= 170
+
+weight = 180
+weight = 160
+weight = "160"
+
+age = 25
+age = 23
+age = "25"
+
+// TYPE STRING
+let myName = "Muhammad Brian"
+let catName:string = "Catty"
+
+myName =true;
+myName = "Muhammad Brian".length
+myName = "Muhammad Brian".length === 0
+
+// TYPE BOOLEAN
+let isAwesome = false
+let isAwesome2 = "Muhammad Brian".length === 0
+let isBoring:boolean = true
+
