be096a5f3dc79ede5cf8f5a350730d09be09a69e
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-23 14:36:05 +0700

    RUNNING AS TS APP

--- index.ts
+++ index.ts
@@ -1,37 +1,37 @@
-class Car {
-  model: string;
-  color: string;
-  age: number;
-  private peopleInPolicy: string[] = [];
-  readonly manufactureDate: string = "2019-01-05";
+// class Car {
+//   model: string;
+//   color: string;
+//   age: number;
+//   private peopleInPolicy: string[] = [];
+//   readonly manufactureDate: string = "2019-01-05";
 
-  constructor(model, color, age) {
-    this.model = model;
-    this.color = color;
-    this.age = age;
-  }
+//   constructor(model, color, age) {
+//     this.model = model;
+//     this.color = color;
+//     this.age = age;
+//   }
 
-  describe() {
-    console.log(`A ${this.age} year old ${this.color} ${this.model}`);
-  }
+//   describe() {
+//     console.log(`A ${this.age} year old ${this.color} ${this.model}`);
+//   }
 
-  addToPolicy(name: string) {
-    this.peopleInPolicy.push(name);
-  }
+//   addToPolicy(name: string) {
+//     this.peopleInPolicy.push(name);
+//   }
 
-  getPolicy() {
-    console.log(this.peopleInPolicy);
-    // myJaguar.manufactureDate = "2019-07-05"; cannot because readonly
-  }
-}
+//   getPolicy() {
+//     console.log(this.peopleInPolicy);
+//     // myJaguar.manufactureDate = "2019-07-05"; cannot because readonly
+//   }
+// }
 
-const myJaguar = new Car("Jaguar", "Red", 4);
-myJaguar.addToPolicy("Muhammad Brian");
-// myJaguar.peopleInPolicy.push("ivan"); cannot because private
-// myJaguar.manufactureDate = "2019-07-05"; cannot because readonly
-myJaguar.manufactureDate;
+// const myJaguar = new Car("Jaguar", "Red", 4);
+// myJaguar.addToPolicy("Muhammad Brian");
+// // myJaguar.peopleInPolicy.push("ivan"); cannot because private
+// // myJaguar.manufactureDate = "2019-07-05"; cannot because readonly
+// myJaguar.manufactureDate;
 
-myJaguar.getPolicy();
+// myJaguar.getPolicy();
 
 //////////////////////////////////////////////
 
@@ -54,3 +54,7 @@ class Vehicle {
     // myJaguar.manufactureDate = "2019-07-05"; cannot because readonly
   }
 }
+
+const myJag = new Vehicle("Jaguar", "green", 25);
+myJag.addToPolicy("Brian");
+myJag.getPolicy();
